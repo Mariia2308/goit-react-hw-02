ef0ddea32c0b0cef3fe790193b8462b56b608191
Mariia2308 <136304734+Mariia2308@users.noreply.github.com>
2024-05-01 00:48:27 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Options from './components/Options/Options';
 import Feedback from './components/Feedback/Feedback';
 import Description from './components/Description/Description';
@@ -9,8 +8,7 @@ import css from './App.module.css';
 function App() {  
   const initialNote = { good: 0, neutral: 0, bad: 0 }
 
-  
-//Local Storage parcing and back
+  // States
   const [TapNote, setTapNote] = useState(() => {
     const stringifyNotes = localStorage.getItem("noteVal")
     const parsedNotes = JSON.parse(stringifyNotes) ?? initialNote
@@ -21,37 +19,38 @@ function App() {
     localStorage.setItem('noteVal', JSON.stringify(TapNote))
   }, [TapNote])
 
-//calculations
-  
+  // Calculations
   const clickTapNote = (note) => {
     setTapNote({ ...TapNote, [note]: TapNote[note] + 1 })
   }
+
   const totalFeedback = TapNote.good + TapNote.neutral + TapNote.bad;
   const positiveFeedback = Math.round(((TapNote.good + TapNote.neutral) / totalFeedback) * 100)
 
-
-//buttons
+  // Buttons
   const toggleOptionsVisibility = () => {
+    // Your toggle functionality here
   }
 
   const handleResetOptions = () => {
-    setTapNote(initialNote)
+    setTapNote(initialNote);
   }
 
   return (
     <div className={css.wrapper}>
       <Description />
-        <div className="card">
-          <Options onToggleOption={toggleOptionsVisibility}
-            clickTapNote={clickTapNote}
-            resetBtn={handleResetOptions}
-            total={totalFeedback}
+      <div className="card">
+        <Options 
+          onToggleOption={toggleOptionsVisibility}
+          clickTapNote={clickTapNote}
+          resetBtn={handleResetOptions}
+          total={totalFeedback}
         />
       </div>
-      { (<Feedback notes={TapNote} total={totalFeedback} positiveFeedback = {positiveFeedback} />
-        
+      {totalFeedback > 0 && (
+        <Feedback notes={TapNote} total={totalFeedback} positiveFeedback={positiveFeedback} />
       )}
-      {totalFeedback === 0 && <Notification /> }
+      {totalFeedback === 0 && <Notification />}
     </div>
   );
 }
